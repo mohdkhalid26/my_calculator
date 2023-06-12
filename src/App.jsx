948e8e75e4704faf1a65e96c7c2cce994104a3e0
mohdkhalid26 <mohdkhalid16170@gmail.com>
2023-06-12 20:10:05 +0530

    four commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,27 @@ import Buttons from "./Buttons";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [dis1, setDis1] = useState("");
+  function getNum() {
+    const numlist = localStorage.getItem("myNum");
+    if (numlist) {
+      return JSON.parse(numlist);
+    } else {
+      return "";
+    }
+  }
+
+  const [dis1, setDis1] = useState(getNum());
+  // const [dis2, setDis2] = useState(getNum());
 
   const onc = (e) => {
     const val = e.target.value;
     setDis1(dis1.concat(val));
+    // setDis2([...dis2, val]);
+    localStorage.setItem("myNum", JSON.stringify(dis1));
   };
-
+  useEffect(() => {
+    localStorage.setItem("myNum", JSON.stringify(dis1));
+  }, [dis1]);
   const sum = () => {
     const charatF = dis1.charAt(0);
     const charatL = dis1.charAt(dis1.length - 1);
@@ -33,16 +47,28 @@ function App() {
       return;
     if (dis1 === "") return;
     setDis1(eval(dis1).toString());
+    // setDis2(eval(dis1).toString());
+    // setDis1("");
   };
 
   const clear = () => {
     setDis1(dis1.substring(dis1.length - 1, dis1.lastIndexOf(" ")));
-    console.log(dis1);
+    // if (dis2.length > 2) {
+    // setDis2(dis2.splice(dis2.length - 1, 1));
+    // } else {
+    // setDis2([]);
+    // }
   };
 
   const allclear = () => {
+    localStorage.clear();
     setDis1("");
+    // setDis2([]);
   };
+
+  // useEffect(() => {
+
+  // }, onc);
 
   return (
     <div className="maindiv">
